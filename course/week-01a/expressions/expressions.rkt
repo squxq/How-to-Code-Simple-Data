f5cc27587f1ed24e49c1101bb6da654c7bb72ed9
root <root@DESKTOP-3H9JI3S>
2023-09-11 20:46:47 +0100

    feat: expressions section files added

---
+++ course/week-01a/expressions/expressions.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname expressions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;(+ 3 4) - comment symbol ;
+
+(+ 3 (* 2 3))
+
+(/ 12 (* 2 3))
+
+(sqr 3)
+
+(sqrt 16)
