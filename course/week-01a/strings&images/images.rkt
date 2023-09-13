7a8124da3cccec0d7f1f2fab098e754d584aee29
squxq <franciscotomas0430@gmail.com>
2023-09-13 12:43:37 +0100

    style: added comments to strings and images section

--- course/week-01a/strings&images/images.rkt
+++ course/week-01a/strings&images/images.rkt
@@ -3,18 +3,26 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname images) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 
-(circle 10 "solid" "red")
+(circle 10 "solid" "red") 
+; creates a red solid (filled) circle with radius 5
+
 (rectangle 30 60 "outline" "blue")
+; creates a blue outlined rectangle with dimensions 30x60
+
 (text "hello" 24 "orange")
+; creates an orange text in font 24 that says: "hello"
 
 (above (circle 10 "solid" "red")
        (circle 20 "solid" "yellow")
        (circle 30 "solid" "green"))
+; creates 3 circles each one of top of another
 
 (beside (circle 10 "solid" "red")
        (circle 20 "solid" "yellow")
        (circle 30 "solid" "green"))
+; creates 3 circles side by side from left to right
 
 (overlay (circle 10 "solid" "red")
        (circle 20 "solid" "yellow")
        (circle 30 "solid" "green"))
+; creates 3 circles stacked on top of each other
--- course/week-01a/strings&images/strings.rkt
+++ course/week-01a/strings&images/strings.rkt
@@ -2,20 +2,30 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname strings) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 "apple"
+; outputs apple
 
 (string-append "Ada" " " "Lovelace")
+; outputs Ada Lovelace
 
-"123"
+"123" ; This is a string that happens to have the characters 1, 2, and 3 in it. 
 
-(+ 1 123)
+123 ; And this is the number, 123.
 
-; (+ 1 "123")
+(+ 1 123) ; In particular, I could take the number and add 1 to it.
+; outputs 124
+
+; (+ 1 "123") - I can't take the string and add 1 to it.
+; outputs +: expects a number, given "123"
 
 (string-length "apple")
+; outputs 5
 
            "0123456789"
 (substring "Caribou" 2 4)
+; outputs "ri"
 
 (substring "0123456789" 2 4)
+; outputs "23"
 
 (substring "Caribou" 0 3)
+; outputs "Car"
