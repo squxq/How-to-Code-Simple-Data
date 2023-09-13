9eadc342f749a6c4165becc9c6b43c576a46f2be
squxq <franciscotomas0430@gmail.com>
2023-09-13 17:27:00 +0100

    feat: using the stepper section

---
+++ course/week-01a/stepper/stepper.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname stepper) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(+ (* 3 2) 1)
+; outputs 7
+
+(define (max-dim img)
+  (if (> (image-width img) (image-height img))
+      (image-width img)
+      (image-height img)))
+
+(max-dim (rectangle 10 20 "solid" "blue"))
+; outputs 20 
