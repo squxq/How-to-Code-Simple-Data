751ccfe4fa28a2655559812e328d11ccfd46ee6d
squxq <franciscotomas0430@gmail.com>
2023-09-22 18:02:59 +0100

    feat: domain analysis module files

---
+++ course/week-03/domainAnalysis/domainAnalysis.no-image.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname domainAnalysis.no-image) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;PROBLEM:
+;
+;Use the How to Design Worlds recipe to design an interactive
+;program in which a cat starts at the left edge of the display 
+;and then walks across the screen to the right. When the cat
+;reaches the right edge it should just keep going right off 
+;the screen.
+;
+;Once your design is complete revise it to add a new feature,
+;which is that pressing the space key should cause the cat to
+;go back to the left edge of the screen. When you do this, go
+;all the way back to your domain analysis and incorporate the
+;new feature.
+;
+;To help you get started, here is a picture of a cat (open the image file), which we
+;have taken from the 2nd edition of the How to Design Programs 
+;book on which this course is based.
